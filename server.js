8fc43d74bf127d6dc463b8986e901116c7ec602f
Riza Fahmi <rizafahmi@gmail.com>
2017-03-26 10:36:38 +0700

    First route

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ const express = require('express')
 
 const app = express()
 
-app.listen(3000, () => {
+// Handlers
+const appHandler = () => {
   console.log('Magic happen at http://localhost:3000/')
-})
+}
+
+const helloHandler = (req, res) => {
+  res.send('May Node be with you!')
+}
+
+// Routes
+app.get('/', helloHandler)
+
+app.listen(3000, appHandler)
